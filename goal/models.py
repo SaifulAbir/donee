0ac0052ee4a122378e081a09b78949c5a29137dd
matrosdzmehedi <matrosdzmehedi111@gmail.com>
2021-10-10 10:57:27 +0600

    add slug in goal and update requirements.txt

--- goal/models.py
+++ goal/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from Donee.models import DoneeModel
+from django.db.models.signals import pre_save
+from .utils import unique_slug_generator
 
 
 class Goal(DoneeModel):
@@ -9,6 +11,7 @@ class Goal(DoneeModel):
         ('PUBLISH', 'Publish'),]
 
     title = models.CharField(max_length=500)
+    slug = models.SlugField(null=False, blank=False, allow_unicode=True)
     short_description = models.CharField(max_length=800)
     full_description = models.TextField()
     buying_item = models.CharField(max_length=200)
@@ -30,6 +33,14 @@ class Goal(DoneeModel):
 
     def __str__(self):
         return self.title
+
+def pre_save_receiver_goal(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(pre_save_receiver_goal, sender=Goal)
+
+
 
 
 class Media(DoneeModel):
