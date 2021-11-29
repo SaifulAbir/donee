5ec70983d2e8c9d384ee841d8c178d66e2022282
Saiful Islam <saiful.abir20@gmail.com>
2021-11-29 10:25:26 +0600

    wsgi file changed

--- Donee/wsgi.py
+++ Donee/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Donee.settings')
-os.environ["DJANGO_SETTINGS_MODULE"] = "Donee.settings"
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Donee.settings')
+# os.environ["DJANGO_SETTINGS_MODULE"] = "Donee.settings"
 
 application = get_wsgi_application()
