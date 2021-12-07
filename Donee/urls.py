3d507bd9daaeaf6b9a18bb4b85e4140a8613d65e
Saiful Islam <saiful.abir20@gmail.com>
2021-12-07 16:31:20 +0600

    admin url changed

--- Donee/urls.py
+++ Donee/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     path('api/',include('user.urls')),
     path('api/',include('goal.urls')),
     path('api/',include('payment.urls')),
