c889eaf9701ce44e24c57ef594ad183f7ff56f9d
Saiful Islam <saiful.abir20@gmail.com>
2021-12-07 11:49:44 +0600

    from user image added in notification

--- notification/serializers.py
+++ notification/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from notification.models import LiveNotification
+from user.serializers import UserSerializer
 
 
 class LiveNotificationSerializer(serializers.ModelSerializer):
-
+    from_user = UserSerializer()
     class Meta:
         model = LiveNotification
         fields = ['id', 'text', 'type', 'from_user', 'to_user', 'created_at']
