328af87ce90d92d38f88e9a19a8e54c21d94d2e2
Saiful Islam <saiful.abir20@gmail.com>
2021-12-12 11:44:19 +0600

    notification sorting done

--- notification/api.py
+++ notification/api.py
@@ -8,7 +8,7 @@ class LiveNotificationListAPI(ListAPIView):
     serializer_class = LiveNotificationSerializer
 
     def get_queryset(self):
-        return LiveNotification.objects.filter(to_user=self.request.user)
+        return LiveNotification.objects.filter(to_user=self.request.user).order_by('-created_at')
 
 class LiveNotificationUpdateAPI(UpdateAPIView):
     queryset=LiveNotification.objects.all()
